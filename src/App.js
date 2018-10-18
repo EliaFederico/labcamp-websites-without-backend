389c486ec9ce23331d4c9dba6ad5646dd7839fcb
Gianni Puglisi <gi.puglisi@bitmama.it>
2018-10-18 16:14:58 +0200

    group-1

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React, { Component } from "react";
-import * as firebase from "firebase";
-
-import ProductsList from "./components/ProductsList";
-import AddProduct from "./components/AddProduct";
-import NewProduct from "./components/NewProduct";
-
-import "./App.css";
-
-// Initialize Firebase
-const config = {
-  apiKey: "AIzaSyD-9xGzU36RhkNmg6B_NW0hMWS3egw_T3o",
-  authDomain: "labcamp-sites-without-backend.firebaseapp.com",
-  databaseURL: "https://labcamp-sites-without-backend.firebaseio.com",
-  projectId: "labcamp-sites-without-backend",
-  storageBucket: "labcamp-sites-without-backend.appspot.com",
-  messagingSenderId: "985712486505"
-};
-firebase.initializeApp(config);
 
 class App extends Component {
   render = () => {
     return (
-      <div>
-        <ProductsList />
-        <AddProduct />
-        <NewProduct />
-      </div>
+      <h1>
+        Welcome to <i>"Building Simple Websites Without Backend"</i> Labcamp
+      </h1>
     );
   };
 }
